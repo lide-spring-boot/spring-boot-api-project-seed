9a145b43ff866b297bb0cb8f5d380a722cd902ae
LiDes <lideshengai@163.com>
2019-09-19 13:49:32 +0800

    添加工具类

--- src/test/java/codeGen/CodeGenerator.java
+++ src/test/java/codeGen/CodeGenerator.java
@@ -20,10 +20,10 @@ import static com.company.project.core.constant.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/demo?useUnicode=true&useSSL=false&useLegacyDatetimeCode=false&allowMultiQueries=true&characterEncoding=utf-8&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&useSSL=false&useLegacyDatetimeCode=false&allowMultiQueries=true&characterEncoding=utf-8&serverTimezone=UTC";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "lds3421597";
-    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
     private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "/src/test/resources/generator/template";//模板位置
@@ -40,7 +40,7 @@ public class CodeGenerator {
     private static boolean isRestful = true;
     public static void main(String[] args) {
 //        genCode("test");
-        genCodeByCustomModelName("account","Account");
+        genCodeByCustomModelName("sys_user","SysUser");
     }
 
     /**
